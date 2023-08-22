52385d303e107f227b40e5f98891113187877c64
HenryCodeT <120686@unsaac.edu.pe>
2023-08-22 15:22:09 -0500

    twitter clone posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { AuthButtonServer } from '@/components/auth-button-server'
+import { PostList } from '@/components/post.list'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { Database } from './types/database'
+import { ComposePost } from '@/components/compose-post'
 
 export default async function Home () {
-  const supabase = createServerComponentClient({ cookies })
+  const supabase = createServerComponentClient<Database>({ cookies })
 
   const { data: { session } } = await supabase.auth.getSession()
 
@@ -12,14 +15,18 @@ export default async function Home () {
     redirect('/login')
   }
 
-  const { data: posts } = await supabase.from('posts').select('*, users(*)')
+  const { data: posts } = await supabase
+    .from('posts')
+    .select('*, user:users(name,avatar_url,user_name)')
+    .order('created_at', { ascending: false })
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AuthButtonServer/>
-      <pre>
-        {JSON.stringify(posts, null, 2)}
-      </pre>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <section className='max-w-[600px] w-full mx-auto border-l border-r border-white/80 min-h-screen'>
+        <ComposePost userAvatarUrl = {session?.user?.user_metadata?.avatar_url} />
+        <PostList posts = {posts}/>
+      </section>
+        <AuthButtonServer/>
     </main>
   )
 }
